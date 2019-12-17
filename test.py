be3d7060d41a93d6509148803e0d73de2dcc997f
Michele Mastrogiovanni <michele.mastrogiovanni@konicaminolta.it>
2019-12-17 14:57:48 +0100

    setup virtual env

--- test.py
+++ test.py
@@ -21,3 +21,9 @@ w = get_weighted_matrix(matrix, 0.1)
 
 bin_matrix = get_binary_matrix(matrix, 0.1)
 
+#------------------------------
+# Algorithms
+#------------------------------
+
+bct.betweenness_bin(bin_matrix)
+
